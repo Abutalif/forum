be782647453ec793646d02076e255ab9c20325c9
Abutalif <abutaliph@gmail.com>
2023-12-30 21:12:01 +0600

    first server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("HELLO")
+	handler := http.NewServeMux()
+	handler.HandleFunc("/", landingPage)
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
+	}
+	server.ListenAndServe()
+}
+
+func landingPage(rw http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		fmt.Println("not get method")
+		return
+	}
+	tpl, err := template.ParseGlob("./assets/templates/*.html")
+	if err != nil {
+		fmt.Println("parse glob: ", err)
+		return
+	}
+	err = tpl.ExecuteTemplate(rw, "index.html", nil)
+	if err != nil {
+		fmt.Println("exec", err)
+	}
 }
